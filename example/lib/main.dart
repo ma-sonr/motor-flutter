080db97537966dc28d84bd1c388e2b16dfd0fffc
Prad Nukala <pradn@sonr.io>
2022-08-20 15:45:43 -0400

    Fix main page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,10 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:alert/alert.dart';
 import 'package:motor_flutter/motor_flutter.dart';
-
-const methods = ["initialize", "createAccount", "login", "createSchema", "queryWhatIs", "address", "balance", "didDoc"];
+import 'package:motor_flutter_example/pages/action_page.dart';
+import 'package:motor_flutter_example/pages/pay_page.dart';
+import 'package:motor_flutter_example/pages/user_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,76 +18,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _motorFlutterPlugin = MotorFlutter();
+  late final MotorFlutter motor;
+
+  @override
+  void initState() {
+    motor = MotorFlutter();
+    motor.initialize();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Motor Flutter Tester'),
-        ),
-        body: ListView.builder(
-          itemCount: methods.length,
-          itemBuilder: ((context, index) {
-            return ListTile(
-              title: Text(methods[index]),
-              onTap: () => _onTap(context, _motorFlutterPlugin, index),
-            );
-          }),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: const Text('Motor Eureka'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.functions)),
+                Tab(icon: Icon(Icons.monetization_on)),
+                Tab(icon: Icon(Icons.person)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ActionPage(motor: motor),
+              PayPage(motor: motor),
+              UserPage(motor: motor),
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  Future<void> _onTap(BuildContext context, MotorFlutter motor, int idx) async {
-    switch (idx) {
-      case 0:
-        final res = await motor.initialize();
-        Alert(message: "Response: $res").show();
-        if (kDebugMode) {
-          print(res);
-        }
-        break;
-      case 1:
-        final res = await motor.createAccount("password");
-        Alert(message: "Response: $res").show();
-        if (kDebugMode) {
-          print(res);
-        }
-        break;
-      case 2:
-        final didDoc = await motor.didDoc();
-        final res = await motor.login(didDoc, "password");
-        Alert(message: "Response: $res").show();
-        if (kDebugMode) {
-          print(res);
-        }
-        break;
-      case 3:
-        final addr = await motor.address();
-        Alert(message: "Address: $addr").show();
-        if (kDebugMode) {
-          print(addr);
-        }
-        break;
-      case 4:
-        final addr = await motor.didDoc();
-        Alert(message: "DIDDocr: $addr").show();
-        if (kDebugMode) {
-          print(addr);
-        }
-        Alert(message: "Not Implemented in Dart.").show();
-        break;
-      case 5:
-        Alert(message: "Not Implemented in Dart.").show();
-        break;
-      case 6:
-        Alert(message: "Not Implemented in Dart.").show();
-        break;
-      case 7:
-        Alert(message: "Not Implemented in Dart.").show();
-        break;
-    }
   }
 }
