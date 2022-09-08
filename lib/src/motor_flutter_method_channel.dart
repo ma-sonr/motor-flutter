e8b22bd8cde4fe9100f81ec8c6b467163c73defc
Prad Nukala <pradn@sonr.io>
2022-09-07 23:43:33 -0500

    Update Motor method channel

--- lib/src/motor_flutter_method_channel.dart
+++ lib/src/motor_flutter_method_channel.dart
@@ -1,11 +1,9 @@
 import 'dart:core';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:protobuf/protobuf.dart';
 import '../motor_flutter.dart';
 import 'gen/registry/tx.pb.dart';
 import 'motor_flutter_platform_interface.dart';
-import 'utilities/information.dart';
 
 /// An implementation of [MotorFlutterPlatform] that uses method channels.
 class MethodChannelMotorFlutter extends MotorFlutterPlatform {
